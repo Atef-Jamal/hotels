dd5a2bfb6d3043a23c75ca8bcf230e2680c3963e
atef gamal <atefgmal778@gmail.com>
2024-08-13 19:33:03 +0300

    commited

--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -1,6 +1,6 @@
-// import { connectToDB } from "@/lib/database";
+import { connectToDB } from "@/lib/database";
 
 export default async function page() {
-  // await connectToDB();
+  await connectToDB();
   return <div>This is Test Page</div>;
 }
