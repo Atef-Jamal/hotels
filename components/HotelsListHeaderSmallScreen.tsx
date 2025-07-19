a58197bd4b460afcb7eba078b3aac1e30c8b5c6a
atef gamal <atefgmal778@gmail.com>
2025-07-19 16:43:34 +0300

    commit

--- components/HotelsListHeaderSmallScreen.tsx
+++ components/HotelsListHeaderSmallScreen.tsx
@@ -1,42 +1,72 @@
 "use client";
 import { cn } from "@/lib/utils";
 import { ArrowDown } from "lucide-react";
-import { useAppContext } from "@/context/context";
 import SelectPriceRatingDrawer from "./SelectPriceRatingDrawer";
 import SelectDistinationDialog from "./SelectDistinationDialog";
 import SelectDatesDialog from "./SelectDatesDialog";
 import ResponsiveSortHotels from "./ResponsiveSortHotels";
 import { DrawerTrigger } from "./ui/drawer";
 import { DialogTrigger } from "./ui/dialog";
+import { useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 const HotelsListHeaderSmallScreen = () => {
-  const { options } = useAppContext();
+  const searchParams = useSearchParams();
+
+  const today = new Date().toISOString().split("T")[0];
+  const tomorrow = new Date(new Date().setDate(new Date().getDate() + 1)).toISOString().split("T")[0];
+
+  const [hotelName, setHotelName] = useState(searchParams.get("hotelName") || "");
+  const [country, setCountry] = useState(searchParams.get("country") || "");
+  const [city, setCity] = useState(searchParams.get("city") || "");
+  const [checkIn, setCheckIn] = useState(searchParams.get("checkIn") || today);
+  const [checkOut, setCheckOut] = useState(searchParams.get("checkOut") || tomorrow);
+  const [minPrice, setMinPrice] = useState(Number(searchParams.get("minPrice")));
+  const [maxPrice, setMaxPrice] = useState(Number(searchParams.get("maxPrice")));
+  const [averageRating, setAverageRating] = useState(Number(searchParams.get("averageRating")));
+
   return (
     <header className="sticky top-0 z-[1] bg-[#623af3] py-2 md:hidden">
       <div className="mx-2 mb-2 rounded-sm bg-white px-2 py-1">
-        <SelectDistinationDialog>
+        <SelectDistinationDialog
+          hotelName={hotelName}
+          country={country}
+          city={city}
+          setHotelName={setHotelName}
+          setCountry={setCountry}
+          setCity={setCity}
+        >
           <DialogTrigger
             className={cn(
               "w-full border-b py-1 text-left text-xs font-medium",
-              !options.hotelName &&
-                !options.location?.city &&
-                !options.location?.country &&
-                "text-muted-foreground",
+              hotelName && city && country && "text-muted-foreground",
             )}
           >
-            {options.hotelName || options.location?.country || options.location?.city || "Enter destination"}
+            {hotelName || country || city || "Enter destination"}
           </DialogTrigger>
         </SelectDistinationDialog>
-        <SelectDatesDialog>
+        <SelectDatesDialog
+          checkIn={checkIn}
+          checkOut={checkOut}
+          setCheckIn={setCheckIn}
+          setCheckOut={setCheckOut}
+        >
           <DialogTrigger className="flex w-full items-center gap-x-4 py-1 text-xs font-medium">
-            <p>{options.checkIn}</p>
+            <p>{checkIn}</p>
             <small className="h-5 w-5 border-b-blue-800 font-semibold text-blue-600">To</small>
-            <p>{options.checkOut}</p>
+            <p>{checkOut}</p>
           </DialogTrigger>
         </SelectDatesDialog>
       </div>
       <div className="flex flex-nowrap gap-x-2 overflow-auto px-3 scrollbar-thin">
-        <SelectPriceRatingDrawer>
+        <SelectPriceRatingDrawer
+          minPrice={minPrice}
+          maxPrice={maxPrice}
+          averageRating={averageRating}
+          setMinPrice={setMinPrice}
+          setMaxPrice={setMaxPrice}
+          setAverageRating={setAverageRating}
+        >
           <DrawerTrigger className="flex items-center justify-center rounded-sm bg-white px-[6px] py-[3px] text-[13px] font-medium text-blue-950">
             Filter
             <ArrowDown size={16} className="ml-1 min-w-fit" />
