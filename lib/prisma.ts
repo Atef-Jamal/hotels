f66a002515a903a40bfe9b24f6402ec26688b95a
Atef Jamal <atefgmal778@gmail.com>
2026-05-22 07:40:49 +0300

    "postinstall": "prisma generate", script added to package.json

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,24 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
+// import { PrismaClient } from "../app/generated/prisma/client";
+
+// const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+
+// const prisma = new PrismaClient({ adapter });
+
+// export default prisma;
+
 import { PrismaClient } from "../app/generated/prisma/client";
-
-const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
-
-const prisma = new PrismaClient({ adapter });
-
+// import { PrismaPg } from "@prisma/adapter-pg";
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 export default prisma;
